20c371d87e8d791c8787bb30c29f0e09583c821c
sky <140752918+AkashskyX@users.noreply.github.com>
2023-12-25 18:47:36 +0530

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // Your javascript code goes here
+</script>
+
+<section class="flex items-center justify-center min-h-screen bg-black m-4 p-4 rounded-md">
+  <h1 class="text-center text-xl mt-4 font-mono font-bold text-white">Hi Dad </h1>
+</section>
